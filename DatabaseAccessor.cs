44297b7b643e4471f7d9889ff9ae243bbbd564c5
jameswangg <37317180+jameswangg@users.noreply.github.com>
2018-05-15 23:23:56 +0900

    Add files via upload

---
+++ DatabaseAccessor.cs
@@ -0,0 +1,23 @@
+﻿using FinalProject.Repository;
+
+namespace FinalProject.Repository
+{
+    public class DatabaseAccessor
+    {
+        private static readonly ClassesDbEntities entities;
+
+        static DatabaseAccessor()
+        {
+            entities = new ClassesDbEntities();
+            entities.Database.Connection.Open();
+        }
+
+        public static ClassesDbEntities Instance
+        {
+            get
+            {
+                return entities;
+            }
+        }
+    }
+}
---
+++ UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace FinalProject.Repository
+{
+    public interface IUserRepository
+    {
+        UserModel LogIn(string email, string password);
+        UserModel Register(string email, string password);
+    }
+
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class UserRepository : IUserRepository
+    {
+        public UserModel LogIn(string email, string password)
+        {
+            var user = DatabaseAccessor.Instance.Users
+                .FirstOrDefault(t => t.UserEmail.ToLower() == email.ToLower()
+                                      && t.UserHashedPassword == password);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            return new UserModel { Id = user.UserId, Name = user.UserEmail };
+        }
+
+        public UserModel Register(string email, string password)
+        {
+            var user = DatabaseAccessor.Instance.Users
+                    .Add(new FinalProject.Repository.User { UserEmail = email, UserHashedPassword = password });
+
+            DatabaseAccessor.Instance.SaveChanges();
+
+            return new UserModel { Id = user.UserId, Name = user.UserEmail };
+        }
+    }
+}
